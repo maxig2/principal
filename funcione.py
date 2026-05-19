b14a2e4d8ef379bb5d1f821f5fd6d06972b385cf
maxig2 <maximlianogomez2007@gmail.com>
2026-05-19 13:03:09 -0300

    Validar_Rangos

--- funcione.py
+++ funcione.py
@@ -11,4 +11,23 @@ resultado = es_par(1)
 
 print(resultado)
 
+def Validar_Rangos (valor_minimo: int, 
+                    valor_maximo: int) -> bool:
 
+    '''
+    '''
+    
+    valor_usuario = int(input("ingrese un valor: "))
+
+    if valor_usuario <= valor_maximo and valor_usuario >= valor_minimo:
+        return True
+    else:
+        Recall = Validar_Rangos(0 , 10)
+        return False
+
+
+
+
+test = Validar_Rangos(0 , 10)
+
+print(test)
